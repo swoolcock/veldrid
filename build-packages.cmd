b2c5a325e8f6fd5afa3d198403fee6efc19c9cf4
Shane Woolcock <samahnub@gmail.com>
2019-11-30 13:08:00 +1030

    Update CI version

--- build-packages.cmd
+++ build-packages.cmd
@@ -5,13 +5,13 @@ dotnet restore src\Veldrid.sln
 
 set UseStableVersions=true
 
-dotnet pack -c Release src\Veldrid.OpenGLBindings\Veldrid.OpenGLBindings.csproj
-dotnet pack -c Release src\Veldrid.MetalBindings\Veldrid.MetalBindings.csproj
-dotnet pack -c Release src\Veldrid\Veldrid.csproj
-dotnet pack -c Release src\Veldrid.Utilities\Veldrid.Utilities.csproj
-dotnet pack -c Release src\Veldrid.ImGui\Veldrid.ImGui.csproj
-dotnet pack -c Release src\Veldrid.ImageSharp\Veldrid.ImageSharp.csproj
-dotnet pack -c Release src\Veldrid.SDL2\Veldrid.SDL2.csproj
-dotnet pack -c Release src\Veldrid.StartupUtilities\Veldrid.StartupUtilities.csproj
-dotnet pack -c Release src\Veldrid.VirtualReality\Veldrid.VirtualReality.csproj
-dotnet pack -c Release src\Veldrid.RenderDoc\Veldrid.RenderDoc.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.OpenGLBindings\Veldrid.OpenGLBindings.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.MetalBindings\Veldrid.MetalBindings.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid\Veldrid.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.Utilities\Veldrid.Utilities.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.ImGui\Veldrid.ImGui.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.ImageSharp\Veldrid.ImageSharp.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.SDL2\Veldrid.SDL2.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.StartupUtilities\Veldrid.StartupUtilities.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.VirtualReality\Veldrid.VirtualReality.csproj
+dotnet pack -c Release /p:Version=%APPVEYOR_BUILD_VERSION% src\Veldrid.RenderDoc\Veldrid.RenderDoc.csproj
